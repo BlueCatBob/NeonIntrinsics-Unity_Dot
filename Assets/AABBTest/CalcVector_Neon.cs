b1c88c0c5c3eeb6d5b7a3f846091847305a3e56f
PengBo <3455405995@qq.com>
2022-12-01 10:42:20 +0800

    Init Commit

---
+++ Assets/AABBTest/CalcVector_Neon.cs
@@ -0,0 +1,251 @@
+﻿
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using static Unity.Burst.Intrinsics.Arm.Neon;
+
+public struct NeonVector3
+{
+    public float[] v3x1;
+    public float[] v3y1;
+    public float[] v3z1;
+    public float[] v3x2;
+    public float[] v3y2;
+    public float[] v3z2;
+
+    public int Length { get; private set; }
+    public NeonVector3(int length)
+    {
+        Length = length;
+        v3x1 = new float[length];
+        v3y1 = new float[length];
+        v3z1 = new float[length];
+        v3x2 = new float[length];
+        v3y2 = new float[length];
+        v3z2 = new float[length];
+    }
+}
+
+public unsafe struct NeonVector3Result
+{
+    public float* v3x;
+    public float* v3y;
+    public float* v3z;
+
+    public int Length { get; private set; }
+    public NeonVector3Result(float* v3x, float* v3y, float* v3z, int length)
+    {
+        Length = length;
+        this.v3x = v3x;
+        this.v3y = v3y;
+        this.v3z = v3z;
+    }
+}
+
+
+[BurstCompile]
+public static class CalcVector_Neon
+{
+    [BurstCompile]
+    public static unsafe void DotProductNeon(float* fArr1, float* fArr2, float* result, int length, int randomCount)
+    {
+        if (IsNeonSupported)
+        {
+            float* fptr1 = fArr1;
+            float* fptr2 = fArr2;
+
+            const int elementsPerIteration = 1;
+            int iterations = length / elementsPerIteration;
+
+            for (int j = 0; j < randomCount; j++)
+            {
+                for (int i = 0; i < iterations; i++)
+                {
+                    //var v1 = vld1_f32(fptr1);
+                    //var v2 = vld1_f32(fptr2);
+                    //var temp = vmulx_f32(v1, v2);
+                    //result[i] = vaddv_f32(temp);
+
+                    //fptr1 += elementsPerIteration;
+                    //fptr2 += elementsPerIteration;
+                    result[i] = vmulxs_f32(fptr1[i], fptr2[i]);
+                }
+            }
+
+        }
+    }
+
+    public static unsafe void DotProductWitoutBurst(float* fArrX1, float* fArrX2, float* fArrY1, float* fArrY2, float* fArrZ1, float* fArrZ2, float* resultX, float* resultY, float* resultZ, int length, int randomCount)
+    {
+        const int elementsPerIteration = 1;
+        int iterations = length / elementsPerIteration;
+        for (int j = 0; j < randomCount; j++)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                resultX[i] = fArrX1[i] * fArrX2[i];
+                resultY[i] = fArrY1[i] * fArrY2[i];
+                resultZ[i] = fArrZ1[i] * fArrZ2[i];
+            }
+        }
+    }
+
+
+    [BurstCompile]
+    public static unsafe void DotProductBurst(float* fArrX1, float* fArrX2, float* fArrY1, float* fArrY2, float* fArrZ1, float* fArrZ2, float* resultX, float* resultY, float* resultZ, int length, int randomCount)
+    {
+        const int elementsPerIteration = 1;
+        int iterations = length / elementsPerIteration;
+        for (int j = 0; j < randomCount; j++)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                resultX[i] = fArrX1[i] * fArrX2[i];
+                resultY[i] = fArrY1[i] * fArrY2[i];
+                resultZ[i] = fArrZ1[i] * fArrZ2[i];
+            }
+        }
+    }
+
+    [BurstCompile]
+    public static unsafe void DotProductNeon(float* fArrX1, float* fArrX2, float* fArrY1, float* fArrY2, float* fArrZ1, float* fArrZ2, float* resultX, float* resultY, float* resultZ, int length, int randomCount)
+    {
+        const int elementsPerIteration = 128 / 32;
+        int iterations = length / elementsPerIteration;
+        if (IsNeonSupported)
+        {
+            var tempV128 = new v128();
+            for (int j = 0; j < randomCount; j++)
+            {
+                float* fptrX1 = fArrX1;
+                float* fptrX2 = fArrX2;
+
+                float* fptrY1 = fArrY1;
+                float* fptrY2 = fArrY2;
+                float* fptrZ1 = fArrZ1;
+                float* fptrZ2 = fArrZ2;
+                for (int i = 0; i < iterations; i++)
+                {
+                    var v11 = vld1q_f32(fptrX1);
+                    var v12 = vld1q_f32(fptrX2);
+                    var v21 = vld1q_f32(fptrY1);
+                    var v22 = vld1q_f32(fptrY2);
+                    var v31 = vld1q_f32(fptrZ1);
+                    var v32 = vld1q_f32(fptrZ2);
+                    tempV128 = vmulq_f32(v11, v12);
+                    resultX[i * elementsPerIteration] = tempV128.Float0;
+                    resultX[i * elementsPerIteration + 1] = tempV128.Float1;
+                    resultX[i * elementsPerIteration + 2] = tempV128.Float2;
+                    resultX[i * elementsPerIteration + 3] = tempV128.Float3;
+                    tempV128 = vmulq_f32(v21, v22);
+                    resultY[i * elementsPerIteration] = tempV128.Float0;
+                    resultY[i * elementsPerIteration + 1] = tempV128.Float1;
+                    resultY[i * elementsPerIteration + 2] = tempV128.Float2;
+                    resultY[i * elementsPerIteration + 3] = tempV128.Float3;
+                    tempV128 = vmulq_f32(v31, v32);
+                    resultZ[i * elementsPerIteration] = tempV128.Float0;
+                    resultZ[i * elementsPerIteration + 1] = tempV128.Float1;
+                    resultZ[i * elementsPerIteration + 2] = tempV128.Float2;
+                    resultZ[i * elementsPerIteration + 3] = tempV128.Float3;
+                    fptrX1 += elementsPerIteration;
+                    fptrX2 += elementsPerIteration;
+                    fptrY1 += elementsPerIteration;
+                    fptrY2 += elementsPerIteration;
+                    fptrZ1 += elementsPerIteration;
+                    fptrZ2 += elementsPerIteration;
+                }
+            }
+
+        }
+    }
+
+    [BurstCompile]
+    public static unsafe void SumNeon(float* fArr1, float* fArr2, float* fArr3, float* result, int length)
+    {
+        const int elementsPerIteration = 1;
+        int iterations = length / elementsPerIteration;
+        if (IsNeonSupported)
+        {
+            float* fptr1 = fArr1;
+            float* fptr2 = fArr2;
+            float* fptr3 = fArr3;
+
+
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var v1 = vld1_f32(fptr1);
+                var v2 = vld1_f32(fptr2);
+                var v3 = vld1_f32(fptr3);
+                var sum23 = vadd_f32(v2, v3);
+                var sum123 = vadd_f32(v1, sum23);
+                result[i] = sum123.Float0;
+                fptr1 += elementsPerIteration;
+                fptr2 += elementsPerIteration;
+                fptr3 += elementsPerIteration;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                result[i] = fArr1[i] + fArr2[i] + fArr3[i];
+            }
+        }
+    }
+
+
+
+    static NeonVector3 GenerateNeonV3(Vector3[] v3array1, Vector3[] v3array2, int length)
+    {
+        NeonVector3 t;
+        if (length != v3array1.Length || length != v3array2.Length || length <= 0)
+        {
+            return new NeonVector3(0);
+        }
+        t = new NeonVector3(length);
+        for (int i = 0; i < length; i++)
+        {
+            t.v3x1[i] = v3array1[i].x;
+            t.v3y1[i] = v3array1[i].y;
+            t.v3z1[i] = v3array1[i].z;
+
+            t.v3x2[i] = v3array2[i].x;
+            t.v3y2[i] = v3array2[i].y;
+            t.v3z2[i] = v3array2[i].z;
+        }
+        return t;
+    }
+
+    public static NeonVector3 GenerateNeonV3(Vector3 v3_1, Vector3 v3_2)
+    {
+        NeonVector3 t = new NeonVector3(1);
+        t.v3x1[0] = v3_1.x;
+        t.v3y1[0] = v3_1.y;
+        t.v3z1[0] = v3_1.z;
+
+        t.v3x2[0] = v3_2.x;
+        t.v3y2[0] = v3_2.y;
+        t.v3z2[0] = v3_2.z;
+        return t;
+    }
+
+    static unsafe Vector3[] TransformToV3(NeonVector3Result neonV3Result)
+    {
+        Vector3[] tArray = new Vector3[neonV3Result.Length];
+        Vector3 t = Vector3.zero;
+        for (int i = 0; i < neonV3Result.Length; i++)
+        {
+            t.x = *(neonV3Result.v3x);
+            t.y = *(neonV3Result.v3y);
+            t.z = *(neonV3Result.v3z);
+
+            tArray[i] = t;
+
+            neonV3Result.v3x++;
+            neonV3Result.v3y++;
+            neonV3Result.v3z++;
+        }
+        return tArray;
+    }
+}
---
+++ Assets/ResultDisplay.cs
@@ -0,0 +1,292 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Unity.Profiling;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum FunctionType
+{
+    WithoutBurst,
+    Burst,
+    Neon
+}
+public class ResultDisplay : MonoBehaviour
+{
+    public Text m_Text;
+
+
+    public Text test;
+
+    int seed = 123456789;
+    static float maxInclude = 100.0f;
+    static float minInclude = -100.0f;
+    static int targetCount = 1000000;
+
+
+    static float _randomSum = 0;
+    static float _randomDotSum = 0;
+
+    bool _isInit;
+
+    Stopwatch _sw;
+    private void Awake()
+    {
+
+        Random.InitState(seed);
+        _isInit = false;
+        _sw = new Stopwatch();
+    }
+
+    void Start()
+    {
+        // Ramp length and number of trials
+        //const int rampLength = 1027;
+        //const int trials = 1000000;
+
+        //m_Text.text = DoCalc(rampLength, trials);
+
+
+
+
+        test.text = DotTest();
+        _isInit = true;
+    }
+
+
+    public unsafe float[] Test(float[] target, FunctionType ft, int randomCount, int length, out float sum)
+    {
+        sum = FloatArraySum(target);
+        float[] reXa = target;
+        float[] reXb = target;
+        float[] reYa = target;
+        float[] reYb = target;
+        float[] reZa = target;
+        float[] reZb = target;
+        float[] resultXSum = new float[length];
+        float[] resultYSum = new float[length];
+        float[] resultZSum = new float[length];
+        float[] result = new float[length];
+        fixed (float* fxa = reXa, fxb = reXb, fya = reYa, fyb = reYb, fza = reZa, fzb = reZb, xSum = resultXSum, ySum = resultYSum, zSum = resultZSum, re = result)
+        {
+            //CalcVector_Neon.DotProductNeon(fxa, fxb, xSum, length);
+            //CalcVector_Neon.DotProductNeon(fya, fyb, ySum, length);
+            //CalcVector_Neon.DotProductNeon(fza, fzb, zSum, length);
+            switch (ft)
+            {
+                case FunctionType.WithoutBurst:
+                    CalcVector_Neon.DotProductWitoutBurst(fxa, fxb, fya, fyb, fza, fzb, xSum, ySum, zSum, length, randomCount);
+
+                    break;
+                case FunctionType.Burst:
+                    CalcVector_Neon.DotProductBurst(fxa, fxb, fya, fyb, fza, fzb, xSum, ySum, zSum, length, randomCount);
+                    break;
+                case FunctionType.Neon:
+                    CalcVector_Neon.DotProductNeon(fxa, fxb, fya, fyb, fza, fzb, xSum, ySum, zSum, length, randomCount);
+                    break;
+                default:
+                    break;
+            }
+            CalcVector_Neon.SumNeon(xSum, ySum, zSum, re, length);
+
+        }
+        return result;
+    }
+
+
+    private unsafe string DoCalc(short rampLength, int trials)
+    {
+        var sb = new StringBuilder();
+
+        // Generate two input vectors
+        // (0, 1, ..., rampLength - 1)
+        // (100, 101, ..., 100 + rampLength-1)
+        var ramp1 = generateRamp(0, rampLength);
+        var ramp2 = generateRamp(100, rampLength);
+        var noBurstPerfMarker = new ProfilerMarker("Not Bursted");
+        var burstPerfMarker = new ProfilerMarker("Bursted");
+
+        fixed (short* ramp1ptr = ramp1, ramp2ptr = ramp2)
+        {
+            int lastResult = 0;
+            var timer = new Stopwatch();
+
+            sb.AppendLine("----==== NO NEON ====----");
+            noBurstPerfMarker.Begin();
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductScalar(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            noBurstPerfMarker.End();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            // Warm-up. First time running bursted version is slower than the subsequent times
+            burstPerfMarker.Begin();
+            timer.Restart();
+            CalculateDotProd.dotProductBurst(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            burstPerfMarker.End();
+            long first = timer.ElapsedMilliseconds;
+
+            sb.AppendLine("----==== NO NEON, Bursted ====----");
+            burstPerfMarker.Begin();
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductBurst(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            burstPerfMarker.End();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {first}, then {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, no unrolling ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 2x unrolling ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon2(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 3x unrolling ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon3(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 4x unrolling ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon4(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 6x unrolling ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon6(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, SMLAL+SMLAL2 2-wide ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon_with_SMLAL2_2wide(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== SMLAL+SMLAL2 4-wide ====----");
+            timer.Restart();
+            lastResult = CalculateDotProd.dotProductNeon_with_SMLAL2_4wide(ramp1ptr, ramp2ptr, rampLength, trials);
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}")
+                .AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    static short[] generateRamp(short startValue, short len)
+    {
+        var ramp = new short[len];
+
+        for (short i = 0; i < len; i++)
+        {
+            ramp[i] = (short)(startValue + i);
+        }
+
+        return ramp;
+    }
+
+
+    static float[] RandomFloatArray(int length)
+    {
+        if (length <= 0)
+        {
+            return new float[0];
+        }
+        float[] tArr = new float[length];
+        for (int i = 0; i < length; i++)
+        {
+            tArr[i] = Random.Range(minInclude, maxInclude);
+        }
+        return tArr;
+    }
+
+    static float FloatArraySum(float[] array)
+    {
+        float sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    string DotTest()
+    {
+
+        int length = 1000;
+        float[] target = RandomFloatArray(length);
+        StringBuilder sb = new StringBuilder();
+        _sw.Restart();
+        float ranSum1;
+        float[] t1 = Test(target, FunctionType.WithoutBurst, targetCount, length, out ranSum1);
+
+
+        sb.AppendLine("TestData Result WithoutBurst RanSum:");
+        sb.AppendLine(ranSum1.ToString());
+        sb.AppendLine("TestData Result FloatArraySum:");
+        float targetRandomDotSum = FloatArraySum(t1);
+        sb.AppendLine(targetRandomDotSum.ToString());
+
+        _sw.Stop();
+        sb.AppendLine("Elapsed Milliseconds:");
+        sb.AppendLine(_sw.ElapsedMilliseconds.ToString() + " ms");
+
+        _sw.Restart();
+        float ranSum2;
+        float[] t2 = Test(target, FunctionType.Burst, targetCount, length, out ranSum2);
+
+        StringBuilder sb1 = new StringBuilder();
+        sb.AppendLine("TestData Result Burst RanSum:");
+        sb.AppendLine(ranSum2.ToString());
+        sb.AppendLine("TestData Result FloatArraySum:");
+        float targetRandomDotSum1 = FloatArraySum(t2);
+        sb.AppendLine(targetRandomDotSum.ToString());
+
+        _sw.Stop();
+        sb.AppendLine("Elapsed Milliseconds:");
+        sb.AppendLine(_sw.ElapsedMilliseconds.ToString() + " ms");
+
+
+        _sw.Restart();
+        float ranSum3;
+        //float[] test = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        //target = test;
+        //length = test.Length;
+        float[] t3 = Test(target, FunctionType.Neon, targetCount, length, out ranSum3);
+
+        StringBuilder sb2 = new StringBuilder();
+        sb.AppendLine("TestData Result Neon RanSum:");
+        sb.AppendLine(ranSum3.ToString());
+        //sb.AppendLine("TestData All Sum Element:");
+        //for (int i = 0; i < t3.Length; i++)
+        //{
+        //    sb.AppendLine(t3[i].ToString());
+        //}
+        float targetRandomDotSum2 = FloatArraySum(t3);
+        sb.AppendLine("TestData Result FloatArraySum:");
+        sb.AppendLine(targetRandomDotSum2.ToString());
+
+        _sw.Stop();
+        sb.AppendLine("Elapsed Milliseconds:");
+        sb.AppendLine(_sw.ElapsedMilliseconds.ToString() + " ms");
+
+        return sb.ToString();
+    }
+}
